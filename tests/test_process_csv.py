1f1eeb5d07b78671db7ff7af1a4be650a68fdd22
Chris Hill-Scott <me@quis.cc>
2016-02-26 16:59:17 +0000

    Handle CSVs with email addresses or phone numbers
    This commit adds a lookup for determining which column of a CSV contains the
    recipient of a message.
    
    The reason for doing this is that ‘mobile number’ is a much better column name
    than ‘to’ if you’re a user who’s struggling to understand what’s going on with
    these CSV things.

--- tests/test_process_csv.py
+++ tests/test_process_csv.py
@@ -5,13 +5,27 @@ from utils.process_csv import get_recipients_from_csv
 
 
 @pytest.mark.parametrize(
-    "file_contents,expected",
+    "file_contents,template_type,expected",
     [
         (
-            'to,name\n+44 123,test1\n+44 456,test2',
+            """
+                phone number,name
+                +44 123,test1
+                +44 456,test2
+            """,
+            'sms',
             ['+44123', '+44456']
+        ),
+        (
+            """
+                email address,name
+                test@example.com,test1
+                test2@example.com,test2
+            """,
+            'email',
+            ['test@example.com', 'test2@example.com']
         )
     ]
 )
-def test_class(file_contents, expected):
-    assert list(get_recipients_from_csv(file_contents)) == expected
+def test_class(file_contents, template_type, expected):
+    assert list(get_recipients_from_csv(file_contents, template_type)) == expected
--- utils/process_csv.py
+++ utils/process_csv.py
@@ -1,10 +1,18 @@
 import csv
 
 
-def get_recipients_from_csv(file_data):
+first_column_heading = {
+    'email': 'email address',
+    'sms': 'phone number'
+}
+
+
+def get_recipients_from_csv(file_data, template_type):
     for row in csv.DictReader(
-        file_data.splitlines(),
+        file_data.strip().splitlines(),
         lineterminator='\n',
         quoting=csv.QUOTE_NONE
     ):
-        yield row['to'].replace(' ', '')
+        yield row[
+            first_column_heading[template_type]
+        ].replace(' ', '')
