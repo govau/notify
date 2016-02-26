c2d8ade3ad8510fba0ce959ba5758a9b6ddaa16c
Chris Hill-Scott <me@quis.cc>
2016-02-26 16:38:33 +0000

    Extract existing code from the API
    Copied from https://github.com/alphagov/notifications-api/blob/10a764a2c140d03000b49181f63b952e7e47de22/app/csv.py

---
+++ tests/test_process_csv.py
@@ -0,0 +1,17 @@
+import pytest
+from io import BytesIO
+
+from utils.process_csv import get_recipients_from_csv
+
+
+@pytest.mark.parametrize(
+    "file_contents,expected",
+    [
+        (
+            'to,name\n+44 123,test1\n+44 456,test2',
+            ['+44123', '+44456']
+        )
+    ]
+)
+def test_class(file_contents, expected):
+    assert get_recipients_from_csv(file_contents) == expected
