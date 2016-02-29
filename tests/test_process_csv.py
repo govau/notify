f941a6a744885d4d7cb3aa55c79bec6b1a1fbc71
Chris Hill-Scott <me@quis.cc>
2016-02-29 10:28:56 +0000

    Refactor into two separate functions

--- tests/test_process_csv.py
+++ tests/test_process_csv.py
@@ -1,7 +1,38 @@
 import pytest
 from io import BytesIO
 
-from utils.process_csv import get_recipients_from_csv
+from utils.process_csv import get_rows_from_csv, get_recipients_from_csv
+
+
+@pytest.mark.parametrize(
+    "file_contents,expected",
+    [
+        (
+            """
+                phone number,name
+                +44 123, test1
+                +44 456,test2
+            """,
+            [
+                {'phone number': '+44 123', 'name': 'test1'},
+                {'phone number': '+44 456', 'name': 'test2'}
+            ]
+        ),
+        (
+            """
+                email address,name
+                test@example.com,test1
+                test2@example.com, test2
+            """,
+            [
+                {'email address': 'test@example.com', 'name': 'test1'},
+                {'email address': 'test2@example.com', 'name': 'test2'}
+            ]
+        )
+    ]
+)
+def test_get_rows(file_contents, expected):
+    assert list(get_rows_from_csv(file_contents)) == expected
 
 
 @pytest.mark.parametrize(
@@ -27,5 +58,5 @@ from utils.process_csv import get_recipients_from_csv
         )
     ]
 )
-def test_class(file_contents, template_type, expected):
+def test_get_recipients(file_contents, template_type, expected):
     assert list(get_recipients_from_csv(file_contents, template_type)) == expected
--- utils/process_csv.py
+++ utils/process_csv.py
@@ -7,13 +7,18 @@ first_column_heading = {
 }
 
 
-def get_recipients_from_csv(file_data, template_type):
+def get_rows_from_csv(file_data):
     for row in csv.DictReader(
         file_data.strip().splitlines(),
         lineterminator='\n',
         quoting=csv.QUOTE_NONE,
         skipinitialspace=True
     ):
+        yield row
+
+
+def get_recipients_from_csv(file_data, template_type):
+    for row in get_rows_from_csv(file_data):
         yield row[
             first_column_heading[template_type]
         ].replace(' ', '')
