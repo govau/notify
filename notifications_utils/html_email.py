4fa868829a84ae8d13eb588828a0d585283c0d09
Chris Hill-Scott <me@quis.cc>
2016-07-04 10:39:12 +0100

    Add option to omit HTML

--- notifications_utils/html_email.py
+++ notifications_utils/html_email.py
@@ -9,11 +9,13 @@ email_template = Environment(loader=FileSystemLoader(
 
 class HTMLEmail():
 
-    def __init__(self, govuk_banner=True):
+    def __init__(self, govuk_banner=True, complete_html=True):
         self.govuk_banner = govuk_banner
+        self.complete_html = complete_html
 
     def __call__(self, body):
         return email_template.render({
             'body': body,
-            'govuk_banner': self.govuk_banner
+            'govuk_banner': self.govuk_banner,
+            'complete_html': self.complete_html
         })
--- tests/test_html_email.py
+++ tests/test_html_email.py
@@ -7,6 +7,13 @@ def test_html_email_inserts_body():
 
 
 @pytest.mark.parametrize(
+    "content", ('DOCTYPE', 'html', 'body', 'GOV.UK', 'hello world')
+)
+def test_default_template(content):
+    assert content in HTMLEmail()('hello world')
+
+
+@pytest.mark.parametrize(
     "show_banner", (True, False)
 )
 def test_govuk_banner(show_banner):
@@ -15,3 +22,17 @@ def test_govuk_banner(show_banner):
         assert "GOV.UK" in email
     else:
         assert "GOV.UK" not in email
+
+
+@pytest.mark.parametrize(
+    "complete_html", (True, False)
+)
+@pytest.mark.parametrize(
+    "content", ('DOCTYPE', 'html', 'body')
+)
+def test_complete_html(complete_html, content):
+    email = HTMLEmail(complete_html=complete_html)('hello world')
+    if complete_html:
+        assert content in email
+    else:
+        assert content not in email
