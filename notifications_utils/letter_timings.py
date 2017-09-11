109ef28ef84c6cc9ca6c2e9b38a90cc5272a8ce2
Chris Hill-Scott <me@quis.cc>
2017-09-11 14:30:16 +0100

    Make delivery estimates default to 4pm
    The delivery estimate should be able to give you a reasonable chance of
    guessing whether a letter has been delivered or not.
    
    According to Royal Mail:
    
    > We aim to deliver to addresses in urban areas by 3pm every working day
    > and addresses in rural areas by 4pm every working day
    
    – http://www.royalmail.com/sites/default/files/docs/pdf/106831_col_del_pdf_02.pdf..unpublished
    
    So if you wait until after 4pm you can be reasonably sure that a letter
    has been delivered.
    
    Only changing this because the API _has_ to return an hour (to be a
    valid timestamp) so it might as well return the most sensible hour I can
    think of.

--- notifications_utils/letter_timings.py
+++ notifications_utils/letter_timings.py
@@ -6,6 +6,10 @@ from collections import namedtuple
 from notifications_utils.timezones import utc_string_to_aware_gmt_datetime
 
 
+def set_gmt_hour(day, hour):
+    return day.astimezone(pytz.timezone('Europe/London')).replace(hour=hour, minute=0)
+
+
 def get_letter_timings(upload_time):
 
     LetterTimings = namedtuple(
@@ -26,12 +30,12 @@ def get_letter_timings(upload_time):
         }.get(processing_day.strftime('%A'), (1, 3, 4))
     )
 
-    printed_by = print_day.astimezone(pytz.timezone('Europe/London')).replace(hour=15, minute=0)
+    printed_by = set_gmt_hour(print_day, hour=15)
     now = datetime.utcnow().replace(tzinfo=pytz.timezone('Europe/London'))
 
     return LetterTimings(
         printed_by=printed_by,
         is_printed=(now > printed_by),
-        earliest_delivery=earliest_delivery,
-        latest_delivery=latest_delivery,
+        earliest_delivery=set_gmt_hour(earliest_delivery, hour=16),
+        latest_delivery=set_gmt_hour(latest_delivery, hour=16),
     )
--- tests/test_letter_timings.py
+++ tests/test_letter_timings.py
@@ -14,94 +14,94 @@ from notifications_utils.letter_timings import get_letter_timings
         '2017-07-10 00:00:01',
         'Tuesday 15:00',
         True,
-        'Thursday 2017-07-13',
-        'Friday 2017-07-14'
+        'Thursday 2017-07-13 16:00',
+        'Friday 2017-07-14 16:00'
     ),
     #  Monday at 16:59 BST
     (
         '2017-07-10 15:59:59',
         'Tuesday 15:00',
         True,
-        'Thursday 2017-07-13',
-        'Friday 2017-07-14'
+        'Thursday 2017-07-13 16:00',
+        'Friday 2017-07-14 16:00'
     ),
     #  Monday at 17:00 BST
     (
         '2017-07-10 16:00:01',
         'Wednesday 15:00',
         True,
-        'Friday 2017-07-14',
-        'Saturday 2017-07-15'
+        'Friday 2017-07-14 16:00',
+        'Saturday 2017-07-15 16:00'
     ),
     #  Tuesday before 17:00 BST
     (
         '2017-07-11 12:00:00',
         'Wednesday 15:00',
         True,
-        'Friday 2017-07-14',
-        'Saturday 2017-07-15'
+        'Friday 2017-07-14 16:00',
+        'Saturday 2017-07-15 16:00'
     ),
     #  Wednesday before 17:00 BST
     (
         '2017-07-12 12:00:00',
         'Thursday 15:00',
         True,
-        'Saturday 2017-07-15',
-        'Monday 2017-07-17'
+        'Saturday 2017-07-15 16:00',
+        'Monday 2017-07-17 16:00'
     ),
     #  Thursday before 17:00 BST
     (
         '2017-07-13 12:00:00',
         'Friday 15:00',
         True,  # WRONG
-        'Monday 2017-07-17',
-        'Tuesday 2017-07-18'
+        'Monday 2017-07-17 16:00',
+        'Tuesday 2017-07-18 16:00'
     ),
     #  Friday anytime
     (
         '2017-07-14 00:00:00',
         'Monday 15:00',
         False,
-        'Wednesday 2017-07-19',
-        'Thursday 2017-07-20'
+        'Wednesday 2017-07-19 16:00',
+        'Thursday 2017-07-20 16:00'
     ),
     (
         '2017-07-14 12:00:00',
         'Monday 15:00',
         False,
-        'Wednesday 2017-07-19',
-        'Thursday 2017-07-20'
+        'Wednesday 2017-07-19 16:00',
+        'Thursday 2017-07-20 16:00'
     ),
     (
         '2017-07-14 22:00:00',
         'Monday 15:00',
         False,
-        'Wednesday 2017-07-19',
-        'Thursday 2017-07-20'
+        'Wednesday 2017-07-19 16:00',
+        'Thursday 2017-07-20 16:00'
     ),
     #  Saturday anytime
     (
         '2017-07-14 12:00:00',
         'Monday 15:00',
         False,
-        'Wednesday 2017-07-19',
-        'Thursday 2017-07-20'
+        'Wednesday 2017-07-19 16:00',
+        'Thursday 2017-07-20 16:00'
     ),
     #  Sunday before 1700 BST
     (
         '2017-07-15 15:59:59',
         'Monday 15:00',
         False,
-        'Wednesday 2017-07-19',
-        'Thursday 2017-07-20'
+        'Wednesday 2017-07-19 16:00',
+        'Thursday 2017-07-20 16:00'
     ),
     #  Sunday after 17:00 BST
     (
         '2017-07-16 16:00:01',
         'Tuesday 15:00',
         False,
-        'Thursday 2017-07-20',
-        'Friday 2017-07-21'
+        'Thursday 2017-07-20 16:00',
+        'Friday 2017-07-21 16:00'
     ),
 
     # GMT
@@ -111,16 +111,16 @@ from notifications_utils.letter_timings import get_letter_timings
         '2017-01-02 16:59:59',
         'Tuesday 15:00',
         True,
-        'Thursday 2017-01-05',
-        'Friday 2017-01-06',
+        'Thursday 2017-01-05 16:00',
+        'Friday 2017-01-06 16:00',
     ),
     #  Monday at 17:00 GMT
     (
         '2017-01-02 17:00:01',
         'Wednesday 15:00',
         True,
-        'Friday 2017-01-06',
-        'Saturday 2017-01-07',
+        'Friday 2017-01-06 16:00',
+        'Saturday 2017-01-07 16:00',
     ),
 
 ])
@@ -134,5 +134,5 @@ def test_get_estimated_delivery_date_for_letter(
     timings = get_letter_timings(upload_time)
     assert timings.printed_by.strftime('%A %H:%M') == expected_print_time
     assert timings.is_printed == is_printed
-    assert timings.earliest_delivery.strftime('%A %Y-%m-%d') == expected_earliest
-    assert timings.latest_delivery.strftime('%A %Y-%m-%d') == expected_latest
+    assert timings.earliest_delivery.strftime('%A %Y-%m-%d %H:%M') == expected_earliest
+    assert timings.latest_delivery.strftime('%A %Y-%m-%d %H:%M') == expected_latest
