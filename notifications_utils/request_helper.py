3b6c75f40a8f34a2ecba376000da22b6f396a3dc
Athanasios Voutsadakis <athanasios.voutsadakis@digital.cabinet-office.gov.uk>
2017-11-11 00:09:25 +0000

    Add helper function to check secret header value
    Also rename request_id -> request_helper to be more generic and
    accurate and update references in existing tests.

--- notifications_utils/request_helper.py
+++ notifications_utils/request_helper.py
@@ -45,3 +45,27 @@ class ResponseHeaderMiddleware(object):
 def init_app(app):
     app.request_class = CustomRequest
     app.wsgi_app = ResponseHeaderMiddleware(app.wsgi_app, 'NotifyRequestID')
+
+
+def check_proxy_header_secret(request, secrets, header='X-Custom-Forwarder'):
+    if header not in request.headers:
+        return False, "Header missing"
+
+    header_secret = request.headers.get(header)
+    if not header_secret:
+        return False, "Header exists but is empty"
+
+    # if there isn't any non-empty secret configured we fail closed
+    if not any(secrets):
+        return False, "Secrets are not configured"
+
+    key_used = None
+    for i, secret in enumerate(secrets):
+        if header_secret == secret:
+            key_used = i + 1  # add 1 to make it human-compatible
+            break
+
+    if key_used is None:
+        return False, "Header didn't match any keys"
+
+    return True, key_used
