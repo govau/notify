14292f161538d9aa133b65af94749485ec283d42
Peter <dagoof@gmail.com>
2018-09-05 13:15:50 +1000

    feat(twilio): support twilio as sms provider
    add Twilio as a provider; so that we can scale to production workloads when the time comes.
    
    It was necessary to unlock utils versions requirements so that we could upgrade/install new dependencies

---
+++ api/scripts/export-ngrok.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import requests
+
+if __name__ == '__main__':
+    response = requests.get('http://localhost:4040/api/tunnels').json()
+
+    for tunnel in response.get('tunnels', []):
+        if tunnel['proto'] != 'https':
+            continue
+
+        if tunnel['config']['addr'] != 'localhost:6011':
+            continue
+
+        sys.stdout.write(tunnel['public_url'])
+        break
