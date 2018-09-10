d2b2baeba11bd22df50bca9fe5623faab0108b59
Andrew Oh <andrewoh531@gmail.com>
2018-09-11 09:06:36 +1000

    Fixing status attempt 1

--- api/tests/app/status/test_status.py
+++ api/tests/app/status/test_status.py
@@ -8,6 +8,6 @@ def test_get_status_all_ok(client):
     resp_json = json.loads(response.get_data(as_text=True))
     assert resp_json['status'] == 'ok'
     assert resp_json['db_version']
-    assert resp_json['travis_commit']
-    assert resp_json['travis_build_number']
+    assert resp_json['commit_sha']
+    assert resp_json['build_number']
     assert resp_json['build_time']
