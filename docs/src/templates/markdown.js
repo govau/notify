e2fb68efe61c842e7ceff77d3dd4a0d3b12e4127
Peter DeGroff <peter.degroff@digital.gov.au>
2018-09-27 18:32:29 +1000

    feat(layout): re-use layout across blog pages

--- docs/src/templates/markdown.js
+++ docs/src/templates/markdown.js
@@ -1,20 +1,20 @@
 import React from 'react'
 import { graphql } from 'gatsby'
 
+import Layout from '../components/layout'
+
 export default ({ data }) => {
   const { frontmatter, html } = data.markdown
 
   return (
-    <div className="blog-post-container">
-      <div className="blog-post">
-        <h1>{frontmatter.title}</h1>
-        <h2>{frontmatter.date}</h2>
-        <div
-          className="blog-post-content"
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
-      </div>
-    </div>
+    <Layout>
+      <h1>{frontmatter.title}</h1>
+      <h2>{frontmatter.date}</h2>
+      <div
+        className="blog-post-content"
+        dangerouslySetInnerHTML={{ __html: html }}
+      />
+    </Layout>
   )
 }
 
