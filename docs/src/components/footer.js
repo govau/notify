743f67db936389d346d39d7f130f24ba148b0f1a
Andrew Oh <andrewoh531@gmail.com>
2018-10-16 10:27:57 +1100

    Styling the footer

--- docs/src/components/footer.js
+++ docs/src/components/footer.js
@@ -1,55 +1,106 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Flex, Box } from '@rebass/grid'
+import { height, space } from 'styled-system'
 
 import { PanelProvider, Wrapper } from './theme'
-import dta from '../images/dta-wordmark.png'
+import dta from '../images/dta-wordmark-white.svg'
+import { External } from './link'
 
 const Logo = styled.img`
-  height: 4rem;
+  padding-left: 0;
+  margin-bottom: 2rem;
+  ${height};
 `
 
 const Root = styled.footer`
   background: ${props => props.theme.background};
   color: ${props => props.theme.content};
+  padding-top: 2rem;
+  padding-bottom: 5rem;
 `
 
-const Columns = styled.div`
+const StyledFlex = styled(Flex)`
+  flex-wrap: wrap;
   margin: 4rem 0;
-  display: flex;
-  justify-content: start;
-  align-items: start;
 
-  & > * + * {
-    margin-left: 4rem;
-  }
+  justify-content: space-between;
+  align-items: start;
+  border-bottom: 1px solid #bfc1c3;
+  padding-bottom: 4rem;
+
+  ${space};
 `
 
 const Ul = styled.ul`
+  padding-left: 0;
   margin-top: 0;
   margin-bottom: 0;
+  list-style-type: none;
 `
+
+const UnorderedList = ({ list }) => (
+  <Box width={[1, 1 / 4]}>
+    <Ul>
+      {list.map(i => (
+        <li key={i.text}>
+          <External blank href={i.target}>
+            {i.text}
+          </External>
+        </li>
+      ))}
+    </Ul>
+  </Box>
+)
+
+const firstColumn = [
+  { text: 'Support', target: 'https://notify.gov.au/support' },
+  { text: 'System status', target: 'https://status.notify.gov.au/' },
+  { text: 'Slack channel', target: 'https://ausdta.slack.com/messages/notify' },
+  { text: 'Blog', target: 'https://dta.gov.au/blog' },
+]
+
+const secondColumn = [
+  { text: 'Features', target: 'https://notify.gov.au/features' },
+  { text: 'Roadmap', target: 'https://notify.gov.au/features/roadmap' },
+  { text: 'Security', target: 'https://notify.gov.au/features/security' },
+  { text: 'Terms of use', target: 'https://notify.gov.au/features/terms' },
+  {
+    text: 'Using Notify',
+    target: 'https://notify.gov.au/features/using-notify',
+  },
+]
+
+const thirdColumn = [
+  { text: 'Pricing', target: 'https://notify.gov.au/pricing' },
+  { text: 'Cookies', target: 'https://notify.gov.au/cookies' },
+  { text: 'Documentation', target: 'https://notify.gov.au/documentation' },
+]
 
 export default props => (
   <PanelProvider>
     <Root>
       <Wrapper>
-        <Columns>
-          <Logo src={dta} />
+        <StyledFlex pr={[0, '10rem']}>
+          <Box width={[1, 1 / 4]}>
+            <Logo src={dta} height={['4rem', '4rem', '6rem']} />
+          </Box>
+          <UnorderedList list={firstColumn} />
+          <UnorderedList list={secondColumn} />
+          <UnorderedList list={thirdColumn} />
+        </StyledFlex>
+      </Wrapper>
 
-          <Ul>
-            <li>these</li>
-            <li>are</li>
-            <li>items</li>
-            <li>that</li>
-            <li>go in the footer</li>
-          </Ul>
-
-          <Ul>
-            <li>and even more</li>
-            <li>footer</li>
-            <li>items</li>
-          </Ul>
-        </Columns>
+      <Wrapper>
+        © Commonwealth of Australia. With the exception of the Commonwealth Coat
+        of Arms and where otherwise noted, this work is licensed under the{' '}
+        <External
+          blank
+          href="https://github.com/govau/notify/blob/master/admin/LICENSE"
+        >
+          MIT license
+        </External>
+        .
       </Wrapper>
     </Root>
   </PanelProvider>
