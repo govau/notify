460d0391f16b233c74963fa05afcc932d89139a6
Jonathan Ingram <jonathan.b.ingram@gmail.com>
2018-11-05 15:51:03 +1100

    feat(celery): add celery beat for scheduled tasks
    We're not currently running any scheduled tasks. This change adds the
    celery beat application to our build.

--- ci/undeploy_closed_branches.py
+++ ci/undeploy_closed_branches.py
@@ -5,7 +5,9 @@ import subprocess
 prefixes = [
     'notify-f-',
     'notify-api-f-',
-    'notify-celery-f-',
+    'notify-celery-f-',  # TODO: can remove once all such applications are cleaned up
+    'notify-celery-worker-f-',
+    'notify-celery-beat-f-',
     'notify-docs-f-',
     'notify-psql-f-',
 ]
