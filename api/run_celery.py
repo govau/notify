ad2491a49c5268bb4a3dd9fbebb1f66e572909e0
Jonathan Ingram <jonathan.b.ingram@gmail.com>
2018-11-06 13:51:50 +1100

    feat(celery): log the type of celery cmd to celery

--- api/run_celery.py
+++ api/run_celery.py
@@ -14,6 +14,9 @@ sentry_sdk.init(
     integrations=[FlaskIntegration()]
 )
 
+with sentry_sdk.configure_scope() as scope:
+    scope.set_tag("celery_cmd", os.environ['NOTIFY_CELERY_CMD'])
+
 application = Flask('delivery')
 create_app(application)
 application.app_context().push()
