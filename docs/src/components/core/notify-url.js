0cdfdde0935ebb6dd769ab5db6f75c26a610bf40
Andrew Oh <andrewoh531@gmail.com>
2018-11-19 10:40:20 +1100

    Address linting warning

--- docs/src/components/core/notify-url.js
+++ docs/src/components/core/notify-url.js
@@ -1,7 +1,7 @@
 const baseUrl = process.env.GATSBY_NOTIFY_BASE_URL || 'https://notify.gov.au'
 
-const ensureOneLeadingSlash = input => `/${(input || '').replace(/^[\/]*/, '')}`
-const stripTrailingSlash = input => (input || '').replace(/[\/]*$/, '')
+const ensureOneLeadingSlash = input => `/${(input || '').replace(/^[/]*/, '')}`
+const stripTrailingSlash = input => (input || '').replace(/[/]*$/, '')
 
 const notifyUrl = relativePath =>
   `${stripTrailingSlash(baseUrl)}${ensureOneLeadingSlash(relativePath)}`
