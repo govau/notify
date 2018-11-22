bcfcfce8ba20e8fe5ea8e7c30f0fc07bdc68f8c7
Dale Hurwitz <dale.hurwitz@digital.gov.au>
2018-11-22 14:16:49 +1100

    Add Array#find polyfill to docs

--- docs/gatsby-browser.js
+++ docs/gatsby-browser.js
@@ -1,7 +1,2 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
-
-// You can delete this file if you're not using it
+// Polyfills
+require('core-js/fn/array/find')
