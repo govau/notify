a51f2e3a2dbffb2fa192eb6817670ff506500fe1
Peter DeGroff <peter.degroff@digital.gov.au>
2019-04-01 14:10:11 +1100

    feat(sentry): track releases using commit sha
    should align release along frontend, admin, celery, api

--- api/application.py
+++ api/application.py
@@ -6,12 +6,16 @@ from flask import Flask
 import sentry_sdk
 from sentry_sdk.integrations.flask import FlaskIntegration
 
-from app import create_app
+from app import create_app, version
+
+sentry_extras = dict(release=version.__commit_sha__)
+sentry_extras = {opt: val for opt, val in sentry_extras.items() if val}
 
 sentry_sdk.init(
     dsn=os.getenv("API_SENTRY_DSN"),
     environment=os.getenv("API_SENTRY_ENV"),
-    integrations=[FlaskIntegration()]
+    integrations=[FlaskIntegration()],
+    **sentry_extras
 )
 
 with sentry_sdk.configure_scope() as scope:
