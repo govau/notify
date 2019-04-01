60b77627cd21e551197427f4e80322c1751a97a9
Andrew Oh <andrewoh531@gmail.com>
2019-04-01 16:05:10 +1100

    Remove anonymize

--- docs/gatsby-config.js
+++ docs/gatsby-config.js
@@ -22,8 +22,6 @@ module.exports = {
         trackingId: "UA-61222473-21",
         // Puts tracking script in the head instead of the body
         head: false,
-        // Setting this parameter is optional
-        anonymize: true,
         // Setting this parameter is also optional
         respectDNT: true,
         // Avoids sending pageview hits from custom paths
