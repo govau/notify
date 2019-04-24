d71443a4d5ae574c7b6cecaa5b9b47194216cfaa
Jonathan Ingram <jonathan.b.ingram@gmail.com>
2019-04-24 16:39:03 +1000

    fix test

--- api/tests/app/platform_stats/test_rest.py
+++ api/tests/app/platform_stats/test_rest.py
@@ -20,23 +20,21 @@ def create_ft_notification_status():
 @freeze_time('2018-06-01')
 def test_get_platform_stats_uses_todays_date_if_no_start_or_end_date_is_provided(admin_request, mocker):
     today = datetime.now().date()
-    dao_mock = mocker.patch('app.dao.services_dao.fetch_aggregate_stats_by_date_range_for_all_services')
-    mocker.patch('app.service.rest.statistics.format_statistics')
+    dao_mock = mocker.patch('app.platform_stats.rest.fetch_aggregate_stats_by_date_range_for_all_services')
 
     admin_request.get('platform_stats.get_platform_stats')
 
-    dao_mock.assert_called_once_with(start_date=today, end_date=today, include_from_test_key=False)
+    dao_mock.assert_called_once_with(start_date=today, end_date=today, include_from_test_key=True)
 
 
 def test_get_platform_stats_can_filter_by_date(admin_request, mocker):
     start_date = date(2017, 1, 1)
     end_date = date(2018, 1, 1)
-    dao_mock = mocker.patch('app.dao.services_dao.fetch_aggregate_stats_by_date_range_for_all_services')
-    mocker.patch('app.service.rest.statistics.format_statistics')
+    dao_mock = mocker.patch('app.platform_stats.rest.fetch_aggregate_stats_by_date_range_for_all_services')
 
     admin_request.get('platform_stats.get_platform_stats', start_date=start_date, end_date=end_date)
 
-    dao_mock.assert_called_once_with(start_date=start_date, end_date=end_date, include_from_test_key=False)
+    dao_mock.assert_called_once_with(start_date=start_date, end_date=end_date, include_from_test_key=True)
 
 
 def test_get_platform_stats_validates_the_date(admin_request):
