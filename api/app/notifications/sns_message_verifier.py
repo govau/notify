0ae23d4415a5c2a0dfcd2505d7bae4d11e986833
Jonathan Ingram <jonathan.b.ingram@gmail.com>
2019-05-20 10:57:59 +1000

    fix vuln notice

--- api/app/notifications/sns_message_verifier.py
+++ api/app/notifications/sns_message_verifier.py
@@ -5,7 +5,6 @@ import requests
 from cryptography import x509
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric.padding import PKCS1v15
-from cryptography.hazmat.primitives.hashes import SHA1
 from cryptography.exceptions import InvalidSignature
 
 
@@ -58,7 +57,7 @@ class SNSMessageVerifier:
                 signature,
                 plaintext,
                 PKCS1v15(),
-                SHA1(),
+                cert.signature_hash_algorithm,
             )
         except InvalidSignature:
             raise SignatureVerificationFailureException('Invalid signature.')
