361819ce152e47aa5b248b2c97867d989166fb80
Peter DeGroff <peter.degroff@digital.gov.au>
2019-07-25 16:49:10 +1000

    docs: fix active lang tab style on initial load

--- docs/src/components/language-selector.js
+++ docs/src/components/language-selector.js
@@ -9,33 +9,47 @@ import { languages } from '../config'
 const Nav = styled.nav`
   display: flex;
   border-bottom: 4px solid #313131;
+
+  & > * + * {
+    margin-left: 2px;
+  }
 `
 
-const LanguageLink = styled.button`
+const NavLanguageLink = styled.button`
   cursor: pointer;
   border: 0;
   background-color: #e8fafc;
 
-  ${props =>
-    props.active
-      ? css`
-          text-decoration: underline;
-          font-weight: bold;
-          background-color: #313131;
-          color: white;
-        `
-      : css``};
-`
-
-// spearate wrapper to get around props interpolation on LanguageLink
-const NavLanguageLink = styled(LanguageLink)`
   padding: 1em 1em;
   flex: 1 1 auto;
   text-align: center;
+`
 
-  & + & {
-    margin-left: 2px;
-  }
+// This component is specifically a `div` and not a `button` because of some
+// issues we've got that seem to go pretty deep.
+//
+// Gatsby will do an initial server render with no access to window. When that
+// happens we have no active tabs and no active styles are applied. On initial
+// client load, the language provider kicks in and reads language from the
+// browser URL. It then might pick up that a tab should be active, but for some
+// reason a proper re-render doesn't occur. React thinks that we've done the
+// render, but no styles are applied. This happens even if we aren't using
+// styled components (just plain react); it also happens if we set a
+// timeout and re-read the active language after the initial render.
+//
+// The only surefire way we've found to trigger a dom update is to swap out
+// to a new element type -- which is why this is a div instead of a button.
+const ActiveNavLanguageLink = styled.div`
+  cursor: pointer;
+  border: 0;
+  text-decoration: underline;
+  font-weight: bold;
+  background-color: #313131;
+  color: white;
+
+  padding: 1em 1em;
+  flex: 1 1 auto;
+  text-align: center;
 `
 
 const createLanguageOption = language => ({
@@ -85,18 +99,30 @@ const Languages = props => (
       <Fragment>
         <H3>Select language</H3>
         <Nav>
-          {languageOptions.map(({ value, label }) => (
-            <NavLanguageLink
-              active={value === current}
-              key={label}
-              onClick={() => {
-                changeLanguage(value)
-                props.onChange(value)
-              }}
-            >
-              {label}
-            </NavLanguageLink>
-          ))}
+          {languageOptions.map(({ value, label }) =>
+            value === current ? (
+              <ActiveNavLanguageLink
+                key={label}
+                tabIndex="0"
+                onClick={() => {
+                  changeLanguage(value)
+                  props.onChange(value)
+                }}
+              >
+                {label}
+              </ActiveNavLanguageLink>
+            ) : (
+              <NavLanguageLink
+                key={label}
+                onClick={() => {
+                  changeLanguage(value)
+                  props.onChange(value)
+                }}
+              >
+                {label}
+              </NavLanguageLink>
+            )
+          )}
         </Nav>
       </Fragment>
     )}
