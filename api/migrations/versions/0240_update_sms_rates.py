56d44da55690409841d8ba452369f2f9a399ea33
Jonathan Ingram <jonathan.b.ingram@gmail.com>
2020-03-23 09:37:33 +1100

    admin: fix incorrect price
    Co-authored-by: Anitha Manickam <ani.manickam@gmail.com>

---
+++ api/migrations/versions/0240_update_sms_rates.py
@@ -0,0 +1,33 @@
+"""
+
+Revision ID: 0240
+Revises: 0239
+Create Date: 2020-03-18 22:13:57.400373
+
+"""
+import uuid
+
+from alembic import op
+import sqlalchemy as sa
+
+
+revision = '0240'
+down_revision = '0239'
+
+
+def upgrade():
+    op.get_bind()
+    op.execute("UPDATE provider_rates SET rate = 6.0 WHERE provider_id = (SELECT id FROM provider_details WHERE identifier = 'telstra')")
+    op.execute("UPDATE provider_rates SET rate = 6.0 WHERE provider_id = (SELECT id FROM provider_details WHERE identifier = 'twilio')")
+    op.execute("UPDATE rates SET rate = 0.06 WHERE valid_from = '2017-06-30 14:00:00'")
+    op.execute("UPDATE rates SET rate = 0.06 WHERE valid_from = '2016-05-18 00:00:00'")
+
+
+def downgrade():
+    op.get_bind()
+    op.execute("UPDATE rates SET rate = 0.0165 WHERE valid_from = '2016-05-18 00:00:00'")
+    op.execute("UPDATE rates SET rate = 0.0158 WHERE valid_from = '2017-06-30 14:00:00'")
+    op.execute("UPDATE provider_rates SET rate = 1.8 WHERE provider_id = (SELECT id FROM provider_details WHERE identifier = 'twilio')")
+    op.execute("UPDATE provider_rates SET rate = 1.8 WHERE provider_id = (SELECT id FROM provider_details WHERE identifier = 'telstra')")
+
+
