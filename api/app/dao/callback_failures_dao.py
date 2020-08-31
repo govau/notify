6519f23cd6c8dc650c3c5cd91bf54623e45cf5a1
Peter DeGroff <peter.degroff@digital.gov.au>
2020-09-01 02:17:35 +1000

    callbacks: throttle when service is failing

--- api/app/dao/callback_failures_dao.py
+++ api/app/dao/callback_failures_dao.py
@@ -1,6 +1,22 @@
 from app import db
 from app.dao.dao_utils import transactional
 from app.models import CallbackFailure
+from sqlalchemy import func
+import datetime
+
+SERVICE_STAT_INTERVAL = 'hour'
+IS_FAILING_MIN_FAILURES = 50
+IS_FAILING_MAX_FAILURES = 500
+
+
+def service_stats_are_failing(*, failure_count, failing_notification_count):
+    if failing_notification_count < IS_FAILING_MIN_FAILURES:
+        return False
+
+    if failure_count > IS_FAILING_MAX_FAILURES:
+        return True
+
+    return False
 
 
 @transactional
@@ -10,3 +26,32 @@ def dao_create_callback_failure(callback_failure):
 
 def dao_get_callback_failures_by_service_id(service_id):
     return CallbackFailure.query.join(CallbackFailure.notification).filter_by(service_id=service_id)
+
+
+def dao_get_callback_failure_service_stats(service_id):
+    return db.session.query(
+        func.count(CallbackFailure.id.distinct()).label('total_failure_count'),
+        func.count(CallbackFailure.notification_id.distinct()).label('failed_notification_count'),
+    ).select_from(
+        CallbackFailure
+    ).filter(
+        CallbackFailure.service_id == service_id,
+    )
+
+
+def dao_get_callback_failure_service_stats_at_time(service_id, current_time):
+    created_started = func.date_trunc(SERVICE_STAT_INTERVAL, CallbackFailure.callback_attempt_started)
+    current_started = func.date_trunc(SERVICE_STAT_INTERVAL, current_time)
+    return dao_get_callback_failure_service_stats(service_id).filter(created_started == current_started)
+
+
+def dao_service_callbacks_are_failing(service_id):
+    failure_stats = dao_get_callback_failure_service_stats_at_time(service_id, datetime.datetime.utcnow()).first()
+
+    if not failure_stats:
+        return False
+
+    return service_stats_are_failing(
+        failure_count=failure_stats.total_failure_count,
+        failing_notification_count=failure_stats.failed_notification_count,
+    )
--- api/app/notifications/callbacks.py
+++ api/app/notifications/callbacks.py
@@ -1,9 +1,11 @@
+from flask import current_app
 from app.config import QueueNames
 from app.celery.service_callback_tasks import (
     send_delivery_status_to_service,
     create_delivery_status_callback_data,
 )
 from app.dao.service_callback_api_dao import get_service_delivery_status_callback_api_for_service
+from app.dao.callback_failures_dao import dao_service_callbacks_are_failing
 
 
 def check_for_callback_and_send_delivery_status_to_service(notification):
@@ -12,8 +14,13 @@ def check_for_callback_and_send_delivery_status_to_service(notification):
     # If a status callback was provided on the notification or if the service
     # has a service_callback_api record, then queue the callback task.
     if (notification.status_callback_url and notification.status_callback_bearer_token) or service_callback_api:
-        encrypted_notification = create_delivery_status_callback_data(notification, service_callback_api)
+        if dao_service_callbacks_are_failing(notification.service_id):
+            current_app.logger.warning(
+                f"send_delivery_status_to_service not called for notification_id: {notification.id} due to failing service_id: {notification.service_id}"
+            )
+            return
 
+        encrypted_notification = create_delivery_status_callback_data(notification, service_callback_api)
         send_delivery_status_to_service.apply_async(
             [str(notification.id), encrypted_notification],
             queue=QueueNames.CALLBACKS,
